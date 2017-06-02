5cbab167ce67a7c16025ed292d59fa53f33bdb8c
David Siaw <davidsiaw@gmail.com>
2017-06-02 15:04:46 +0900

    No strtoq on alpine

--- deps/libpcre/linux/config.h
+++ deps/libpcre/linux/config.h
@@ -125,7 +125,7 @@ sure both macros are undefined; an emulation function will then be used. */
 /* #undef HAVE_STRTOLL */
 
 /* Define to 1 if you have `strtoq'. */
-#define HAVE_STRTOQ 1
+#define HAVE_STRTOQ 0
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
